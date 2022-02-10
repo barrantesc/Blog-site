087805e723ee3b4c397991fe3accbbe028207f4e
playground <connie.s.barrantes@gmail.com>
2022-02-10 16:55:32 -0500

    MCV paragigm to organize code, used handlebars.js expressions and template the homepage layout

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
+//-- access to stylesheet within express app
+const path = require('path');
+
+//-- Express
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//-- Feeding Express server info it needs to be used
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//-- this MUST be above routes
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+//-- Defining APP template engine - Using Handelbars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
