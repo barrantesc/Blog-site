5c04086c20302e05a4787397deb1d40c00ec93e7
playground <connie.s.barrantes@gmail.com>
2022-03-30 20:14:19 -0400

    utils folder

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,22 @@
-const withAuth = (req, res, next) => {
-    if (!req.session.user_id) {
-      res.redirect('/login');
-    } else {
-      next();
+module.exports = {
+  format_date: date => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+      date
+    ).getFullYear()}`;
+  },
+  format_url: url => {
+    return url
+      .replace('http://', '')
+      .replace('https://', '')
+      .replace('www.', '')
+      .split('/')[0]
+      .split('?')[0];
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
     }
-  };
-  
-  module.exports = withAuth;
+
+    return word;
+  }
+};
