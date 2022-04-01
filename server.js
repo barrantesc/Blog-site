f429221ea28d389ceeef6adc31eab951d90be19c
playground <connie.s.barrantes@gmail.com>
2022-04-01 16:35:32 -0400

    updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 //-- access to stylesheet within express app
 const path = require('path');
-
 //-- Express
 const express = require('express');
+// express-session
+const session = require('express-session')
 //-- Defining APP template engine - Using Handelbars
 const exphbs = require('express-handlebars');
+
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
@@ -13,18 +15,17 @@ const PORT = process.env.PORT || 3001;
 
 //-- Feeding Express server info it needs to be used
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 //-- this MUST be above routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
 
-
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
-
 
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, './views/'));
